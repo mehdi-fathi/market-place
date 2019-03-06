76fdd4c59bd04831f96ec7ac20302531a344f350
Mehdi Fathi <mehdifathi.developer@gmail.com>
2019-03-06 07:37:00 +0330

    Make apps folder & fix config phpunit

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,7 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+Route::resource('/task', '\Apps\Http\Admin\Controllers\UsersController');
 Route::get('/', function () {
     try {
         DB::connection()->getPdo();
